707be2d1461d7dffd001129a2c346d48d94697e6
luoliangovo <2628345510@qq.com>
2019-02-13 21:00:04 +0800

    回文数

--- test.py
+++ test.py
@@ -11,3 +11,25 @@ while count <= 10:
 
 
 # swap a b
+a = input("请输入a的值:")
+b = input("请输入b的值:")
+temp = a
+a = b
+b = temp
+print("a的值="+a)
+print("b的值="+b)
+
+# Palindrome Number
+
+def isPalindrome(number):
+	if number < 0:
+		return False
+	else:
+		str1 = str(number)
+		str2 = str1[::-1]
+		if str1 ==  str2:
+			return True
+		else:
+			return False
+result = isPalindrome(int(input('请输入一个数字：'))) 
+print(result)
