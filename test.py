e5fa128e5211403889f196bbc2b9b6d97273d5bf
luoliangovo <2628345510@qq.com>
2019-02-21 20:01:40 +0800

    reverse

--- test.py
+++ test.py
@@ -39,12 +39,12 @@ print(result)
 #
 def romanToInt(s):
 	a = {'I':1,
-             'V':5,
-             'X':10,
-             'L':50,
-             'C':100,
-             'D':500,
-             'M':1000}
+         'V':5,
+         'X':10,
+         'L':50,
+	     'C':100,
+	     'D':500,
+	     'M':1000}
 ans = 0
 for i in range(len(s)):
 	if i ==0 or a[s[i]]<=a[s[i-1]]:
@@ -63,6 +63,23 @@ def twoSum(nums,target):
 		x = target - nums[i]
 		if i in nums and nums.index(x) != i:
 			return [i,nums.index(x)]
+
+
+#Reverse Integer
+
+def reverse(x):
+	max,min = 1<<31,-1<<31
+	if x < 0 :
+		str1 = str(-x)
+		ans = -1*int(str1[::-1])
+	else:
+		str2 = str(x)
+		ans = int(str2[::-1])
+	if ans < min or ans >= max:
+		return 0 
+	return ans
+
+
 
 
 
